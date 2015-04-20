b0d5ddc5bfb94f51d03d412a71f9b9719300f60e
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-20 13:28:48 -0700

    Figured it out...A lot simpler than I  thought thanks to the reading links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-main.js
+$( ".Dan" ).click(function() {
+	//when the div Dan geets clicked
+    alert( "Hey you clicked on me." );
+    //calling placeholder text line 1
+});
+
+
+$( ".Iwao" ).click(function() {
+	//when the div Iwao gets clicked
+    alert( "Hey you clicked on me again." );
+    //calling placeholder text line 2
+});
